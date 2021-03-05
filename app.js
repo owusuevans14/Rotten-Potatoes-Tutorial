a324e09c7a2f417dc1e45cecdcb86baa57fca716
Evans OWUSU <eowusu@user1s-MacBook-Pro.local>
2021-03-05 03:08:13 -0500

    Users can see all reviews

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+//app.get('/', (req, res) => {
+//  res.render('home', { msg: 'Handlebars are Cool!' });
+//})
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('reviews-index', { reviews: reviews });
 })
+
+
+
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
